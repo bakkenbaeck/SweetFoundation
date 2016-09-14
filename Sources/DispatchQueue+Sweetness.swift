9b1dc2de5a83009cac31c1eaa22acad0f2ee439c
Elvis Nuñez <elvisnunez@me.com>
2016-09-14 16:04:21 +0200

    Remove non-Foundation code

--- Sources/DispatchQueue+Sweetness.swift
+++ Sources/DispatchQueue+Sweetness.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension DispatchQueue {
-    public func asyncAfter(seconds: Double, execute block: @escaping () -> Void) {
+    public func asyncAfter(_ seconds: Double, execute block: @escaping () -> Void) {
         let deadline: DispatchTime = DispatchTime.now() + Double(Int64(0.25 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
         self.asyncAfter(deadline: deadline, execute: block)
     }
