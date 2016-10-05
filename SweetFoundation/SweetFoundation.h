cf284ebcb9b39050f9b1e1a5bd711831ee7f4046
Igor Ranieri Elland <igor@elland.me>
2016-10-05 20:59:53 +0200

    Add shared framework scheme

---
+++ SweetFoundation/SweetFoundation.h
@@ -0,0 +1,19 @@
+//
+//  SweetFoundation.h
+//  SweetFoundation
+//
+//  Created by Igor Ranieri on 05/10/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SweetFoundation.
+FOUNDATION_EXPORT double SweetFoundationVersionNumber;
+
+//! Project version string for SweetFoundation.
+FOUNDATION_EXPORT const unsigned char SweetFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SweetFoundation/PublicHeader.h>
+
+
