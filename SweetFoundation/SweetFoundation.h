6ccd90666ef7a5ac2d29995609fc39a023bb5ecc
Elvis Nuñez <3lvis@users.noreply.github.com>
2016-10-26 13:53:30 +0200

    Clean up header

--- SweetFoundation/SweetFoundation.h
+++ SweetFoundation/SweetFoundation.h
@@ -1,19 +1,4 @@
-//
-//  SweetFoundation.h
-//  SweetFoundation
-//
-//  Created by Igor Ranieri on 05/10/2016.
-//
-//
+@import Foundation;
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for SweetFoundation.
 FOUNDATION_EXPORT double SweetFoundationVersionNumber;
-
-//! Project version string for SweetFoundation.
 FOUNDATION_EXPORT const unsigned char SweetFoundationVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <SweetFoundation/PublicHeader.h>
-
-
