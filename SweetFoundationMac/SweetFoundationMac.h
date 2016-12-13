1038ab67ae4bf0b1ec4427f37855ba7370db49a4
Igor Ranieri Elland <igor@elland.me>
2016-12-13 09:58:50 +0100

    Added non-padded Base64 support. Moved tests to Mac target.

---
+++ SweetFoundationMac/SweetFoundationMac.h
@@ -0,0 +1,19 @@
+//
+//  SweetFoundationMac.h
+//  SweetFoundationMac
+//
+//  Created by Igor Ranieri on 13/12/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SweetFoundationMac.
+FOUNDATION_EXPORT double SweetFoundationMacVersionNumber;
+
+//! Project version string for SweetFoundationMac.
+FOUNDATION_EXPORT const unsigned char SweetFoundationMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SweetFoundationMac/PublicHeader.h>
+
+
