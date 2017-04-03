6964859e444194efac19a1be666c6fb65d3ee0e0
Elvis Nuñez <elvisnunez@me.com>
2017-04-03 16:37:50 +0200

    Update tests

---
+++ Tests/Tests.swift
@@ -0,0 +1,64 @@
+import XCTest
+@testable import SweetFoundation
+
+class Tests: XCTestCase {
+
+    // Not sure how to test that the type is correctly inferred.
+    // This pretty much just ensures that the property is accessible and non-nil for a non-empty array.
+    func testArrayAny() {
+        let array: Array = ["Some", "Value", "Random"]
+        let value = array.any
+
+        XCTAssertNotNil(value)
+    }
+
+    func testMath() {
+        var d = Math.degreesToRadians(45)
+        var almostRight = d <= 0.786 && d >= 0.785
+        XCTAssert(almostRight)
+
+        d = Math.degreesToRadians(65)
+        almostRight = d <= 1.135 && d >= 1.134
+        XCTAssert(almostRight)
+
+        d = Math.radiansToDegrees(Double.pi)
+        almostRight = d <= 180.1 && d >= 179.99
+        XCTAssert(almostRight)
+
+        d = Math.radiansToDegrees(Double.pi / 2)
+        almostRight = d <= 90.1 && d >= 89.99
+        XCTAssert(almostRight)
+    }
+
+    func testBase64() {
+        var string = "This is a test string"
+        var string64 = string.data(using: .utf8)!.base64EncodedString()
+        var string64NoPadding = string.data(using: .utf8)!.base64EncodedStringWithoutPadding()
+
+        XCTAssertEqual(string64, "VGhpcyBpcyBhIHRlc3Qgc3RyaW5n")
+        XCTAssertEqual(string64NoPadding, "VGhpcyBpcyBhIHRlc3Qgc3RyaW5n")
+        XCTAssertEqual(string64NoPadding.paddedForBase64, "VGhpcyBpcyBhIHRlc3Qgc3RyaW5n")
+
+        string = "This is a test string without padding"
+        string64 = string.data(using: .utf8)!.base64EncodedString()
+        string64NoPadding = string.data(using: .utf8)!.base64EncodedStringWithoutPadding()
+
+        XCTAssertEqual(string64, "VGhpcyBpcyBhIHRlc3Qgc3RyaW5nIHdpdGhvdXQgcGFkZGluZw==")
+        XCTAssertEqual(string64NoPadding, "VGhpcyBpcyBhIHRlc3Qgc3RyaW5nIHdpdGhvdXQgcGFkZGluZw")
+        XCTAssertEqual(string64NoPadding.paddedForBase64, "VGhpcyBpcyBhIHRlc3Qgc3RyaW5nIHdpdGhvdXQgcGFkZGluZw==")
+
+        string = "One-pad string"
+        string64 = string.data(using: .utf8)!.base64EncodedString()
+        string64NoPadding = string.data(using: .utf8)!.base64EncodedStringWithoutPadding()
+
+        XCTAssertEqual(string64, "T25lLXBhZCBzdHJpbmc=")
+        XCTAssertEqual(string64NoPadding, "T25lLXBhZCBzdHJpbmc")
+        XCTAssertEqual(string64NoPadding.paddedForBase64, "T25lLXBhZCBzdHJpbmc=")
+    }
+
+    func testSafeAccess() {
+        let array = [1, 2, 3]
+
+        XCTAssertEqual(array.element(at: 3), nil)
+    }
+}
