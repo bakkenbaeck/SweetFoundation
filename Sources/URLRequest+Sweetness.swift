2a61b77bd68434e7e613e57a8da0250713a19406
Elvis Nuñez <3lvis@users.noreply.github.com>
2017-04-04 15:12:49 +0200

    Update URLRequest+Sweetness.swift

--- Sources/URLRequest+Sweetness.swift
+++ Sources/URLRequest+Sweetness.swift
@@ -6,7 +6,7 @@ extension URLRequest {
         if let headers = self.allHTTPHeaderFields {
             description.append("\n\(headers)")
         }
-        if let httpBody = self.httpBody, let dataString = String(data: httpBody, encoding: String.Encoding.utf8) {
+        if let httpBody = self.httpBody, let dataString = String(data: httpBody, encoding: .utf8) {
             description.append("\n\(dataString)")
         }
 
