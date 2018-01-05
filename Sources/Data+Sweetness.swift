1e9b1d4a86ed5cd81df465e992a73c22c51f097b
Igor Ranieri Elland <igor@elland.me>
2018-01-05 12:09:26 +0100

    Removed empty space.

--- Sources/Data+Sweetness.swift
+++ Sources/Data+Sweetness.swift
@@ -3,22 +3,22 @@ import Foundation
 public extension Data {
     public func base64EncodedStringWithoutPadding() -> String {
         let base64 = self.base64EncodedString()
-        
+
         if base64.hasSuffix("==") {
-            return String(base64[base64.startIndex..<base64.index(base64.endIndex, offsetBy: -2)])
+            return String(base64[base64.startIndex ..< base64.index(base64.endIndex, offsetBy: -2)])
         } else if base64.hasSuffix("=") {
-            return String(base64[base64.startIndex..<base64.index(base64.endIndex, offsetBy: -1)])
+            return String(base64[base64.startIndex ..< base64.index(base64.endIndex, offsetBy: -1)])
         }
-        
+
         return base64
     }
-    
+
     public func hexadecimalString() -> String {
         var hexString = ""
         for byte in [UInt8](self) {
             hexString += String(format: "%02lx", byte)
         }
-        
+
         return hexString
     }
 }
