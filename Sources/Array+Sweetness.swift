13312d7065b036ca19beae07e0fafb70db71a36a
Ellen Shapiro <designatednerd@gmail.com>
2018-09-21 14:27:09 +0200

    Add deprecation warning to `any` extension and encourage people to just use the stdlib method.

--- Sources/Array+Sweetness.swift
+++ Sources/Array+Sweetness.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 public extension Array {
+
+    @available(*, deprecated, message: "Use the standard library's `.randomElement()` method instead.")
     public var any: Element? {
         return self.randomElement()
     }
