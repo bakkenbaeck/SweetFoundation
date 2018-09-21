dc2c56f2546072e777b38e02bad733e6d4dff137
Ellen Shapiro <designatednerd@gmail.com>
2018-09-21 14:14:11 +0200

    use `randomElement` API instead of `arc4Random_uniform`

--- Sources/Array+Sweetness.swift
+++ Sources/Array+Sweetness.swift
@@ -2,6 +2,6 @@ import Foundation
 
 public extension Array {
     public var any: Element? {
-        return self[Int(arc4random_uniform(UInt32(self.count)))] as Element
+        return self.randomElement()
     }
 }
