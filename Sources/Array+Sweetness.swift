f1d332e1c0e753bc7a7d0dadcc7d07f69459380d
Ellen Shapiro <designatednerd@gmail.com>
2018-09-21 15:19:45 +0200

    clearer deprecation declaration

--- Sources/Array+Sweetness.swift
+++ Sources/Array+Sweetness.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public extension Array {
 
-    @available(*, deprecated, message: "Use the standard library's `.randomElement()` method instead.")
+    @available(swift, deprecated: 4.2, message: "Use the standard library's `.randomElement()` method instead.")
     public var any: Element? {
         return self.randomElement()
     }
